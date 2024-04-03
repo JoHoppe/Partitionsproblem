4fc358f1916880076bc7a77126447c3444561717
JHoppe <sgs3.iloveit@gmail.com>
2024-04-03 10:15:20 +0200

    changed fitness calculation to simple subtraction, now smaller score is better. implemented random set generator and setup basic structure for saving configs

--- algorithm.py
+++ algorithm.py
@@ -1,27 +1,20 @@
 import back_end
 
 
-def run_evolutionary_algorithm(
-        initial_set,
-        amount_solutions,
-        generations,
-        fitness_selection=10,
-        mutation_factor=4,
-        stagnation_threshold=3,
-):
+def run_evolutionary_algorithm(initial_set, amount_solutions, generations, fitness_selection=10, mutation_factor=4,
+                               stagnation_threshold=3, good_enough_number=5):
     # Initialize the population
     population = back_end.initialization(initial_set, amount_solutions)
-    best_solution = back_end.sort_by_fitness(population)[
-        0
-    ]  # Get the best solution from the sorted population
+    best_solution = back_end.sort_by_fitness(population)[0]  # Get the best solution from the sorted population
     stagnation_tracker = 0
 
     # Evolve the population for the specified number of generations
     for gen in range(generations):
         population = back_end.sort_by_fitness(population)
         # if we achieved a good enough solution abort
-        if best_solution.fitness >= 99:  # Check the fitness of the best solution
-            print("found good enough solution")
+        if best_solution.fitness <= good_enough_number:  # Check the fitness of the best solution
+            print(f"found good enough solution: {best_solution.fitness}")
+
             return {"solution": best_solution, "generations_needed": gen + 1}
         if stagnation_tracker >= stagnation_threshold:
             print("stagnation threshold reached")
@@ -40,34 +33,19 @@ def run_evolutionary_algorithm(
         population = back_end.sort_by_fitness(population)
 
         # Update the best solution if necessary
-        if (
-                population[0].fitness > best_solution.fitness
-        ):  # Check the fitness of the best solution
+        if population[0].fitness < best_solution.fitness:  # Check the fitness of the best solution
             best_solution = population[0]
 
     return {"solution": best_solution, "generations_needed": generations}
 
 
-def run_algorithm_multiple_times(
-        initial_set,
-        generation_size,
-        amount_generations,
-        fitness_selection,
-        mutation_factor,
-        stagnation_threshold,
-        num_runs,
-):
+def run_algorithm_multiple_times(initial_set, generation_size, amount_generations, fitness_selection, mutation_factor,
+                                 stagnation_threshold, num_runs, ):
     results = []
 
     for _ in range(num_runs):
-        result = run_evolutionary_algorithm(
-            initial_set,
-            generation_size,
-            amount_generations,
-            fitness_selection,
-            mutation_factor,
-            stagnation_threshold,
-        )
+        result = run_evolutionary_algorithm(initial_set, generation_size, amount_generations, fitness_selection,
+                                            mutation_factor, stagnation_threshold, )
         results.append(result)
 
     return results
