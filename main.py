f2de485547be7d6e1d989b839def589fca9924a1
JHoppe <sgs3.iloveit@gmail.com>
2024-04-05 15:43:03 +0200

    fixed bug with set loading and saving

--- main.py
+++ main.py
@@ -5,24 +5,23 @@ import util
 def main():
     initial_set = util.random_set(100, 10, 400)
     print(initial_set)
-
-    generation_size = 15
-    amount_generations = 50
-    fitness_selection = 5
-    mutation_factor = 1
-    stagnation_threshold = 1
-    num_runs = 10
+    util.save_set(initial_set)
+    set_1 = util.load_set(1)
+    # generation_size = 15
+    # amount_generations = 50
+    # fitness_selection = 5
+    # mutation_factor = 1
+    # stagnation_threshold = 1
+    # num_runs = 10
     good_enough_number = 1
-    run_config = util.RunConfig(generation_size=generation_size, amount_generations=amount_generations,
-                                fitness_selection=fitness_selection, mutation_factor=mutation_factor,
-                                stagnation_threshold=stagnation_threshold, num_runs=num_runs, good_enough_number=good_enough_number)
-    util.save_config(run_config=run_config)
+    # run_config = util.RunConfig(generation_size=generation_size, amount_generations=amount_generations,
+    #                            fitness_selection=fitness_selection, mutation_factor=mutation_factor,
+    #                           stagnation_threshold=stagnation_threshold, num_runs=num_runs, good_enough_number=good_enough_number)
+    #util.save_config(run_config=run_config)
     # TODO: create a way to load and safe configs and initial sets
     # ToDO: mute_factor must be smaller then pop
     run_config2 = util.load_config(1)
-    results = algorithm.run_algorithm_multiple_times(initial_set,
-        *run_config2.values()
-    )
+    results = algorithm.run_algorithm_multiple_times(set_1, *run_config2.values())
     util.plot_generations_needed(results)
 
 
